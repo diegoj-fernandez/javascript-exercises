487242e11bf4cf4aa58558b70645bddef3c7b942
Diego Fernandez <diegoj.fernandez@outlook.com>
2022-05-22 08:40:09 -0400

    Finish exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function(randArray,randElementOne,randElementTwo,randElementThree,randElementFour) {
+    let newArray = [];
 
+    for (let i = 0; i < randArray.length; i++) {
+        if (randArray[i] !== randElementOne && randArray[i] !== randElementTwo && randArray[i] !== randElementThree && randArray[i] !== randElementFour) {
+            newArray.push(randArray[i]);
+        }
+    }
+
+    return newArray;
+
+    /*
+
+    // FIRST DRAFT
+
+    for (let i = 0; i < randArray.length; i++) {
+        if (randArray[i] == randElementOne || randArray[i] == randElementTwo) {
+            randArray.splice(i,1);
+            //return randArray;
+        }
+    }
+    return randArray;
+
+    */
 };
 
 // Do not edit below this line
