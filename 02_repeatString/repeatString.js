b2d6d49c94da91279511cc9b06488f986e0e8844
Diego Fernandez <diegoj.fernandez@outlook.com>
2022-05-22 07:29:42 -0400

    Finish exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,45 @@
-const repeatString = function() {
+const repeatString = function(word,num) {
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        let outputString = "";
 
+        for (let i = 0; i < num; i++) {
+            outputString += word;
+        }
+
+        return outputString;
+    }
+
+    /*
+
+    //FIRST DRAFT
+
+    let string = "hey";
+    let num = 3;
+    return string.repeat(num);
+
+    */
+
+    /*
+
+    //SECOND DRAFT
+
+    let originalString = "hey";
+    let newString = "";
+    let number = 10;
+
+    if (number < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 0; i < number; i++) {
+            newString = newString + originalString;
+        }
+    }
+
+    return newString;
+
+    */
 };
 
 // Do not edit below this line
