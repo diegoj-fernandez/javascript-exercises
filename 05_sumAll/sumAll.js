b40fa9660be4e1c565d009c30a848a669ce63d58
Diego Fernandez <diegoj.fernandez@outlook.com>
2022-05-22 10:26:44 -0400

    Edit code to pass test 5 in sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,7 @@ const sumAll = function(num1,num2) {
     let array1 = [];
     let sum = 0;
 
-    if (num1 < 0 || num2 < 0) {
+    if (typeof(num1) !== "number" || typeof(num2) !== "number" || num1 < 0 || num2 < 0) {
         return "ERROR"
     } else if (num1 < num2) {
         for (let i = num1; i <= num2; i++) {
@@ -17,8 +17,6 @@ const sumAll = function(num1,num2) {
 
         return sum;
     }
-
-    //return sum;
 };
 
 // Do not edit below this line
