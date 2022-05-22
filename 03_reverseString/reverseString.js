f69c1a6a894b4fac2833288e5e8a6c040baa8d5f
Diego Fernandez <diegoj.fernandez@outlook.com>
2022-05-22 07:46:24 -0400

    Finish exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reverseWord = "";
+    for (let i = word.length; i >= 0; i--) {
+        reverseWord += word.slice(i - 1, i);
+    }
 
+    return reverseWord;
 };
 
 // Do not edit below this line
