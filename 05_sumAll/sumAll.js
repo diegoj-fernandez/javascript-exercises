659f6b3530c6fd3698f5a6b967d21527d55957b2
Diego Fernandez <diegoj.fernandez@outlook.com>
2022-05-22 10:20:56 -0400

    Edit code to pass test 4 in sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,24 +2,23 @@ const sumAll = function(num1,num2) {
     let array1 = [];
     let sum = 0;
 
-    if (num1 < num2) {
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR"
+    } else if (num1 < num2) {
         for (let i = num1; i <= num2; i++) {
             sum += i;
-
-            /*
-            if (i <= num2) {
-                sum += i;
-            }
-            */
-
         }
+
+        return sum;
     } else if (num1 > num2) {
         for (let i = num1; i >= num2; i--) {
             sum += i;
         }
+
+        return sum;
     }
 
-    return sum;
+    //return sum;
 };
 
 // Do not edit below this line
