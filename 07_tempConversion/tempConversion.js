e21d6a8c5060fdd0e7308ab92736d523697b2152
Diego Fernandez <diegoj.fernandez@outlook.com>
2022-05-22 11:12:58 -0400

    Pass test 1 in tempConversion.spec.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
+const ftoc = function(numFaren) {
+  let ansCel = (numFaren - 32) * 5 / 9;
 
+  if (ansCel == 0) {
+    return Math.round(ansCel);
+  } else {
+    return ansCel.toFixed(1);
+  }
 };
 
-const ctof = function() {
+const ctof = function(numCel) {
+  let ansFaren = (numCel * 9 / 5) + 32;
 
+  if (ansFaren == 0) {
+    return Math.round(ansFaren);
+  } else {
+    return ansFaren.toFixed(1);
+  }
 };
 
 // Do not edit below this line
