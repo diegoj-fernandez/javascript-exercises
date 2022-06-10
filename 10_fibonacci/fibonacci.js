f5c5a843da8e0f1c5b1331c622ca7a4e8825f17d
Diego Fernandez <diegoj.fernandez@outlook.com>
2022-06-10 16:07:58 -0400

    Pass all tests in fibonacci.spec.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    } else if (n == 1 || n == 2) {
+        return 1;
+    } else {
+        let fiboArray = [1,1];
+        for (let i = 2; i < n; i++) {
+            fiboArray.push(fiboArray[i - 2] + fiboArray[i - 1]);
+        }
+        return fiboArray[fiboArray.length - 1];
+    }
 
+    //could I also use the actual fibonacci equation?
 };
 
 // Do not edit below this line
