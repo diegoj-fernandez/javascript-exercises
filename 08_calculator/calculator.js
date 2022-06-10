b6821204846a42a693160e079dafd3cff40c4cf7
Diego Fernandez <diegoj.fernandez@outlook.com>
2022-06-10 12:35:21 -0400

    Pass all tests in calculator.spec.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,53 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+  if (num1 >= 0 && num2 >= 0) {
+    return num1 + num2;
+  }
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array1) {
+	if (array1 == "") {
+    return 0;
+  } else if (array1.length == 1) {
+    return array1[array1.length - 1];
+  } else {
+    let initial = 0;
+    for (let i = 0; i < array1.length; i++) {
+      initial += array1[i];
+    }
+    return initial;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(array1) {
+  let initial = 1;
+  for (let i = 0; i < array1.length; i++) {
+    initial *= array1[i];
+  }
+  return initial;
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+  let initial = num1;
+	for (let i = 0; i < num2 - 1; i++) {
+    initial *= num1;
+  }
+  return initial;
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	if (num1 == 0 || num1 == 1) {
+    return 1;
+  } else {
+    let array1 = [];
+    for (let i = 1; i <= num1; i++) {
+      array1.push(i);
+    }
+    return multiply(array1);
+  }
 };
 
 // Do not edit below this line
