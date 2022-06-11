85846b7d9aac31311aab5156f28bc8eeffeb8f41
Diego Fernandez <diegoj.fernandez@outlook.com>
2022-06-11 17:25:29 -0400

    Pass all tests in getTheTitles.spec.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,16 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
 
+    //MY SOLUTION
+
+    let titleArray = [];
+    for (let i = 0; i < array.length; i++) {
+        titleArray.push(array[i].title);
+    }
+    return titleArray;
+
+    //TOP SOLUTION
+
+    //return array.map(book => book.title);
 };
 
 // Do not edit below this line
