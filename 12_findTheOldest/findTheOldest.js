2cfbeb841c71dbceed6fd078c5fb4a7530974529
Diego Fernandez <diegoj.fernandez@outlook.com>
2022-06-11 19:58:49 -0400

    Write first draft for findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentYear = new Date().getFullYear();
+    let namesArray = [];
+    let agesArray = [];
 
+    let age;
+    let oldestAge = 0;
+    let oldestPerson;
+
+    for (let i = 0; i < array.length; i++) {
+        namesArray.push(array[i].name);
+        if (array[i].hasOwnProperty("yearOfDeath")) {
+            age = array[i].yearOfDeath - array[i].yearOfBirth;
+            agesArray.push(age);
+        } else {
+            age = currentYear - array[i].yearOfBirth;
+            agesArray.push(age);
+        }
+    }
+
+    for (let i = 0; i < agesArray.length; i++) {
+        if (agesArray[i] >= oldestAge) {
+            oldestAge = agesArray[i];
+            oldestPerson = namesArray[i];
+        }
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
